447772fbe9977d1368d24e83f4aca7c37678a1c1
mustikadianulan <mustikadianulan@gmail.com>
2018-05-09 21:27:46 +0700

    mca

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBbBA3iqghgtpFhu4EkS3kR7YxWwSj4i4I",
+    authDomain: "mca-chat-903b4.firebaseapp.com",
+    databaseURL: "https://mca-chat-903b4.firebaseio.com",
+    projectId: "mca-chat-903b4",
+    storageBucket: "mca-chat-903b4.appspot.com",
+    messagingSenderId: "701614747245"
+  };
+
+  export default config
--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,14 @@ import 'firebase/firestore'
 import users from './users'
 import conversations from './conversations'
 
-import config from '../config'
+const config = {
+    apiKey: "AIzaSyBbBA3iqghgtpFhu4EkS3kR7YxWwSj4i4I",
+    authDomain: "mca-chat-903b4.firebaseapp.com",
+    databaseURL: "https://mca-chat-903b4.firebaseio.com",
+    projectId: "mca-chat-903b4",
+    storageBucket: "mca-chat-903b4.appspot.com",
+    messagingSenderId: "701614747245"
+  };
 
 firebase.initializeApp(config)
 
